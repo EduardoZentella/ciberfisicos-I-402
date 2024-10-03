59f9f7bd60f7169d8242b1f09a2a56dd07af6e5c
victor <vicosmor932@outlook.com>
2024-10-03 03:39:33 -0600

    Actualizacion frontend

--- Frontend/trazabilidad/src/app/UI/Dashboard/Navbar/Navbar.tsx
+++ Frontend/trazabilidad/src/app/UI/Dashboard/Navbar/Navbar.tsx
@@ -1,8 +1,46 @@
-const Navbar = () => {
+'use client';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faCog, faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useLanguage } from '../../../context/LanguageContext';
+import styles from './Navbar.module.css';
+import images from '../../images/smart_factory.png';
+import Image from 'next/image';
+
+const Navbar: React.FC = () => {
+  const { currentTranslations, changeLanguage, currentLanguage } = useLanguage();
+
+  const handleLogout = () => {
+    // Implementar la lógica de cierre de sesión
+    console.log("User logged out");
+  };
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <Image 
+          src={images}
+          alt="SmartTec logo"
+          className={styles.logoImage}
+        />
+        <span className={styles.logoText}>SmartTec</span>
+      </div>
+
+      <div className={styles.accesContainer}>
+
+        <div>Loco </div>
+        <div>LOCO</div>
+        <div className={styles.languageSwitcher}>
+        <button onClick={() => changeLanguage('en')} className={`${styles.languageButton} ${currentLanguage === 'en' ? styles.active : ''}`}>EN</button>
+        <button onClick={() => changeLanguage('es')} className={`${styles.languageButton} ${currentLanguage === 'es' ? styles.active : ''}`}>ES</button>
+      </div>
+      </div>
+   
+      
+     
+      
+      
+    </nav>
   );
 };
 
