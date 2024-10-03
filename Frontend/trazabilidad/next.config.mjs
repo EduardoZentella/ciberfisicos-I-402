c4c98fa8e935939631a4d2861e43afb0394510fd
Eduardo Zentella <A00835387@tec.mx>
2024-10-02 19:58:27 -0600

    Fix Heroku deployment

--- Frontend/trazabilidad/next.config.mjs
+++ Frontend/trazabilidad/next.config.mjs
@@ -1,6 +1,17 @@
 import withYAML from 'next-yaml';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Definir __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withYAML({});
+const nextConfig = withYAML({
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+});
 
 export default nextConfig;
